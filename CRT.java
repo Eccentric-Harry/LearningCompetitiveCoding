20dabb3dbc3829a6c96895808a0c341005409c23
Eccentric Harry <harinathrao13@gmail.com>
2024-08-21 17:03:07 +0530

    practice

---
+++ CRT.java
@@ -0,0 +1,23 @@
+public class CRT{
+    public static int crt(int[] a, int[] m, int k){
+        int x = 1;
+        int j;
+        while(true){
+            for(j = 0; j < k; j++){
+                if(x%m[j]!=a[j]){
+                    break;
+                }
+            }
+            if(j==k){
+                return x;
+            }
+            x++;
+        }
+    }
+    public static void main(String[] args){
+        int[] a = {2,3,2};
+        int[] m = {3,5,7};
+        int k = a.length;
+        System.out.println("x is " + crt(a, m, k));
+    }
+}
