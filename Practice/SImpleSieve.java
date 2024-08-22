ca8fc6c4491f3ef891f22224026e203ce6257c11
EccentricHarry <harinathrao13@gmail.com>
2024-08-22 17:07:04 +0530

    Practice

---
+++ Practice/SImpleSieve.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class SimpleSieve{
+    public static void sieve(int  n){
+        int[] arr = new int[n+1];
+        Arrays.fill(arr, -1);
+
+        for(int i = 2; i <n; i++){
+            if(arr[i]==-1){
+                for(int j = i*i; j <n; j+=i){
+                    arr[j] = 0;
+                }
+            }
+        }
+        for(int i = 2; i <n; i++){
+            if(arr[i] != 0){
+                System.out.print(i+ " ");
+            }
+        }
+    }
+    public static void main(String[] args){
+        int n = 19;
+        sieve(n);
+    }
+}
